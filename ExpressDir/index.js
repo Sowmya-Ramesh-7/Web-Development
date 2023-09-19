2177a5183b4d2757a8079ca732bb1c93cf3e7260
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-09-19 12:13:55 +0530

    Express js
    first server

---
+++ ExpressDir/index.js
@@ -0,0 +1,30 @@
+const express= require("express"); 
+//express stores the function to create web application
+
+//that is to run and it returns an object thats stored in app variable
+const app=express();
+let port=3000;
+
+app.listen(port,()=>{
+    console.log("app is listening");
+})
+
+// app.use((req,res)=>{
+//     console.log("request recieved");
+//     let htmlcode="<h1>This is a html response</h1>"
+//     res.send(htmlcode)
+// });
+
+app.get("/",(req,res)=>{
+    res.send("<h1>This is the root path</h1>");
+});
+app.get("/fruits",(req,res)=>{
+    res.send("<h1>Fruits Page</h1>");
+});
+app.get("*",(req,res)=>{
+    res.send("<h1>This path does not exists</h1>");
+});
+
+app.post("/",(req,res)=>{
+    res.send("<h1>You sent a post request</h1>");
+});
