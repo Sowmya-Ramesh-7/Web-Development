325e891f78469fb6c87668cf4b6237aa403bf1cf
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-09-19 12:13:10 +0530

    Backend using nodejs

---
+++ Backend/math.js
@@ -0,0 +1,5 @@
+export const sum= (a,b) =>(a+b); //exporting individually
+export const diff= (a,b) =>(a-b);
+export const mul= (a,b) =>(a*b);
+export const PI= 3.142;
+
---
+++ Backend/script.js
@@ -0,0 +1,4 @@
+import {sum,PI} from "./math.js" 
+//initialize package and add "type": "module" attribute/key-value pair in package.json file to import  
+console.log(PI);
+console.log(sum(3,4));
